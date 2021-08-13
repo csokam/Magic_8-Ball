4c35d0ca4da697ba84aa7535c9a3009331619ccc
csokam <csokam@users.noreply.github.com>
2021-08-13 23:52:00 +0200

    Added file handling

--- magic_8_main.py
+++ magic_8_main.py
@@ -8,6 +8,8 @@ answers=["It is Certain.","It is decidedly so.","Without a doubt.","Yes definite
 "Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.",
 "Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
 
+f=open("log.csv", "a", encoding="utf-8")
+
 while answered==False:    
     print("Ask your question.")
     question=input()
@@ -16,6 +18,8 @@ while answered==False:
         print("Questions end with question marks.")    
     else:
         os.system("cls")       
-        print("Question:\n"+question)
-        print("Answer:\n"+answers[random.randint(0,19)])
+        answer=answers[random.randint(0,19)]
+        print("Question:\n"+question)        
+        print("Answer:\n"+answer)
+        f.write(question+","+answer+"\n")
         answered=True
