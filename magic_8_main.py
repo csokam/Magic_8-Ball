60cfa1c96395ce2f1240d96656cbc05f8e45220d
csokam <you@example.com>
2022-06-09 23:07:08 +0200

    changed "," to ";" for clarity

--- magic_8_main.py
+++ magic_8_main.py
@@ -3,7 +3,7 @@ import os
 
 answered = False
 question=" "
-answers=["It is Certain.","It is decidedly so.","Without a doubt.","Yes definitely.","You may rely on it.",
+answers=["It is Certain.","It is decidedly so.","Without a doubt.","Yes, definitely.","You may rely on it.",
 "As I see it, yes.","Most likely.","Outlook good.","Yes.","Signs point to yes.",
 "Reply hazy, try again.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.",
 "Don't count on it.","My reply is no.","My sources say no.","Outlook not so good.","Very doubtful."]
@@ -21,5 +21,5 @@ while answered==False:
         answer=answers[random.randint(0,19)]
         print("Question:\n"+question)        
         print("Answer:\n"+answer)
-        f.write(question+","+answer+"\n")
+        f.write(question+";"+answer+"\n")
         answered=True
